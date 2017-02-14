de1012e58ba423a3916947875dc5dca23a3f7b5c
appadmin <appadmin@PD001>
2017-02-14 12:54:50 +0900

    プロジェクト ファイルを追加します。

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace uiui
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PicBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicBox
+            // 
+            this.PicBox.Image = global::uiui.Properties.Resources.hello;
+            this.PicBox.InitialImage = null;
+            this.PicBox.Location = new System.Drawing.Point(0, 0);
+            this.PicBox.Name = "PicBox";
+            this.PicBox.Size = new System.Drawing.Size(468, 264);
+            this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicBox.TabIndex = 0;
+            this.PicBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 266);
+            this.Controls.Add(this.PicBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Welcome";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace uiui
+{
+
+    public partial class FrmMain : Form
+    {
+        private FrmMain Par = null;
+        private bool _IsRunning = false;
+        public bool IsRunning        {            get { return _IsRunning; } }
+
+        [DllImport("winmm", CharSet = CharSet.Auto)]
+        private static extern int mciSendString( string command, StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+
+        public FrmMain(object _par)
+        {
+            InitializeComponent();
+            if(_par != null) { Par = (FrmMain)_par; }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            bool run = true;
+            if (Par != null)
+            {
+                if (Par.IsRunning)
+                {
+                    run = false;
+                }
+            }
+            this.timer1.Enabled = run;
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            FrmMain newFrm = new FrmMain(this);
+            newFrm.Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                DriveInfo di = new DriveInfo(drive);
+                if (di.DriveType == DriveType.CDRom)
+                {
+                    int ret = int.MinValue;
+                    try
+                    {
+                        ret = mciSendString("open " + "\"" + drive  + "\"" + " type cdaudio alias orator", null, 0, IntPtr.Zero);
+                        ret = mciSendString("set orator door open", null, 0, IntPtr.Zero);
+                        ret = mciSendString("close orator", null, 0, IntPtr.Zero);
+                        //ret = mciSendString("Set CDAudio Door Open Wait", null, 0, IntPtr.Zero);
+                    }
+                    catch(Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
